da27249916f48c0cb59cf4ff58c2f9db985e8217
levi <904066757@qq.com>
2025-06-20 10:26:32 +0800

    test

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App";
 import { initSentry, ErrorBoundary } from "./utils/sentry";
 import "./index.css";
 import { testWebSocketConnection } from "./services/testWebSocket";
-// 初始化 Sentry
+// 初始化 Sentry性能监控和错误日志
 initSentry();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
